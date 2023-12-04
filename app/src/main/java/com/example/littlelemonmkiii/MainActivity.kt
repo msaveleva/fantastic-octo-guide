42ecb2cedd92664cf65d55693a2e38c35eaa727c
lyumotech <lyumotech@pm.me>
2023-12-04 16:56:52 +0200

    Ktor setup - completed.

--- app/src/main/java/com/example/littlelemonmkiii/MainActivity.kt
+++ app/src/main/java/com/example/littlelemonmkiii/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.littlelemonmkiii
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.rememberNavController
 import com.example.littlelemonmkiii.navigations.Navigation
+import com.example.littlelemonmkiii.networking.MenuNetwork
 import com.example.littlelemonmkiii.storage.StorageUtil
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
 import io.ktor.http.ContentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val storageUtil: StorageUtil by lazy {
@@ -26,10 +31,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            val menu = getMenu()
+            println(menu)
+        }
+
         setContent {
             val navController = rememberNavController()
             val skipOnboarding = storageUtil.shouldSkipOnboarding()
             Navigation(navController = navController, skipOnboarding = skipOnboarding)
         }
+    }
+
+    private suspend fun getMenu(): MenuNetwork {
+        return client.get(listUrl).body()
     }
 }
